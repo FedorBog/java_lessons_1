ec133d3dba294218a8508f69becbffabe83cfb7e
FedorBog <fedos89@gmail.com>
2023-03-06 15:27:10 +0500

    If

---
+++ src/If.java
@@ -0,0 +1,15 @@
+public class If {
+
+    public static void main(String[] args) {
+
+        int myInt = 15;
+
+        if (myInt < 10){
+            System.out.println("Less than 10");
+        } else if( myInt > 20 ){
+            System.out.println("Bigger than 20");
+        } else{
+            System.out.println("Others");
+        }
+    }
+}
