2a8faba0541d1693b1493d537d74ab3d56848bff
FedorBog <fedos89@gmail.com>
2023-03-06 14:43:39 +0500

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,17 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Strings {
+
+    public static void main(String[] args){
+
+        int age = 56;
+
+        String name = "John";
+        String surName = "Doe";
+        System.out.println(name + " " + surName );
+
+        System.out.println("Age is " + age);
+
+    }
+
+}
