131656cf5407cdc6fd1b9ebc44f31859e18efac4
FedorBog <fedos89@gmail.com>
2023-03-06 18:22:45 +0500

    DoWhile

---
+++ src/DoWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int inputNumb;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 5");
+
+        do {
+
+            inputNumb = Integer.parseInt(input.nextLine());
+
+        }while (inputNumb != 5);
+    }
+}
