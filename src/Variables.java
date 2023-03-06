a312c32ac1528952850e10b115271e5d64aff2bd
FedorBog <fedos89@gmail.com>
2023-03-06 13:48:14 +0500

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,31 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Variables {
+    public static void  main( String[] args ){
+
+        int myInt = 444;
+        System.out.println(myInt);
+
+        short myShort = 111;
+        System.out.println(myShort);
+
+        long myLong = 123123;
+        System.out.println(myLong);
+
+        double myDouble = 12312.232;
+        System.out.println(myDouble);
+
+        float myFloat = 1232.334f;
+        System.out.println(myFloat);
+
+        char myChar = 'F';
+        System.out.println(myChar);
+
+        boolean myBool = true;
+        System.out.println(myBool);
+
+        byte myByte = 122;
+        System.out.println(myByte);
+
+    }
+}
