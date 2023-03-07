1398a6c50918813746e4722064e815d796f93850
FedorBog <fedos89@gmail.com>
2023-03-07 09:41:57 +0500

    Break_Continue

---
+++ src/Break_Continue.java
@@ -0,0 +1,25 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Break_Continue {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        System.out.println("Break");
+        while(true){
+           if (i == 15) {
+               break;
+           }
+           System.out.println(i);
+           i++;
+        }
+
+        System.out.println("Continue");
+        for (i = 0; i <=15; i++){
+           if (i % 2 == 0){
+               continue;
+           }
+            System.out.println(i);
+        }
+    }
+}
