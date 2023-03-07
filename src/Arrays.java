8932e7479eaa9297a710b8e4e068c8247d60a34f
FedorBog <fedos89@gmail.com>
2023-03-07 10:30:12 +0500

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = i*10;
+            System.out.println(numbers[i]);
+        }
+
+        for (int x: numbers) {
+            System.out.println(x);
+        }
+    }
+}
